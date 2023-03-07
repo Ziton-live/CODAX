527a1ce6abf76df3df4adcb6b0fd42c2b8cfc8c5
NIRANJAN B <niranjannb7777@gmail.com>
2023-03-07 11:09:03 +0530

    Threshold Calculation without DOCS

--- src/_threshold_model/gen_attack_threshold.h
+++ src/_threshold_model/gen_attack_threshold.h
@@ -3,6 +3,7 @@
 // #include "../commons.h";
 #include <math.h>
 #include "vmlinux.h"
+#include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_core_read.h>
