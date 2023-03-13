01b9b31c59a80504e1cf7e149dfe62183cf10b10
AJITH-klepsydra <ajithpmuralidharan01@gmail.com>
2023-03-13 15:19:30 +0530

    array based prevention sol

--- src/_monitor/container_tracer.bpf.c
+++ src/_monitor/container_tracer.bpf.c
@@ -1,19 +1,21 @@
-
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+
 #include <bpf/bpf_core_read.h>
 #include <stdbool.h>
 #include "container_tracer.h"
 #include <string.h>
 #include "../commons.h"
 #include <math.h>
+
 // #include "../_threshold_model/gen_attack_threshold.h"
 
 char LICENSE[]
 SEC("license") = "Dual BSD/GPL";
 
 u64 get_cpu_time(u64 elapsed_time);
+
 
 void model_cpu_threshold(u64 elapsed_time, int pid);
 
@@ -28,8 +30,18 @@ struct {
     __type(key,
     int);
     __type(value, u64);
-} pid_map
-SEC(".maps");
+} pid_map SEC(".maps");
+
+
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 10);
+    __type(key, int);
+    __type(value, long);
+} my_array SEC(".maps");
+
+
 
 u64 get_current_time() {
     return bpf_ktime_get_ns();
@@ -41,16 +53,17 @@ bool restrict_to_test() {
 
     char file_name[256];
     bpf_get_current_comm(&file_name,sizeof(file_name));
-     /**
-        @todo Find a a proper way to distinguish container from non-docker processes.
-        */
 
+    /**
+        @todo Find a a proper way to distinguish container from non-docker processes.
+    */
     if(is_it_docker(file_name)){
         return true; 
     }
 
     return false;
 }
+
 
 
 /**
@@ -70,6 +83,13 @@ int bpf_trace_accept_system_call(struct pt_regs *ctx) {
     if (restrict_to_test()) {
         bpf_map_update_elem(&pid_map, &pid, &start_time, BPF_ANY);
     }
+    int pid_, zero = 0;
+    u32 index = 0;
+    long *value;
+    long value_ = 42;
+    bpf_map_update_elem(&my_array, &index, &value_, BPF_ANY);
+    value = bpf_map_lookup_elem(&my_array, &index);
+    bpf_printk("[test]:%d\n",value);
     return 0;
 }
 
@@ -99,12 +119,12 @@ int bpf_trace_close_system_call(struct pt_regs *ctx) {
 }
 
 /**
-
     @brief Calculates the CPU time used for a given elapsed time.
     This function takes as input the elapsed. It is intended to be used to calculate the CPU time used by a process during the elapsed time.
     @warning Dont use this function as it is
     @param elapsed_time The elapsed time for which to calculate the CPU time.
     @return The same value as the input elapsed_time.
+    this 
     */
 u64 get_cpu_time(u64 elapsed_time) {
     return elapsed_time;
@@ -130,44 +150,10 @@ SEC(".maps");
 
 
 void model_cpu_threshold(u64 elapsed_time, int pid) {
-    bpf_printk("[%d] took %llu nano seconds\n: ", pid, elapsed_time);
-
-    int k = 3;
-
-    struct map_value def_val;
-    def_val.thresh = 0;
-    def_val.std = 0;
-    def_val.mean = 0;
-    def_val.t_max = 0;
-    def_val.n = 0;
-
-    struct map_value *value_ptr = bpf_map_lookup_or_init(&thresh_maps, &pid, &def_val);
-
-
-    u64 t_max = value_ptr->t_max;
-    u64 n = value_ptr->n;
-    double mean = value_ptr->mean;
-    double std = value_ptr->std;
-    double thresh = value_ptr->thresh;
-
-    double temp_mean = mean;
-
-    mean = (n * mean + elapsed_time) / n + 1;
-    std = sqrt((n * pow(std, 2) + pow(elapsed_time - temp_mean, 2)) / (n + 1));
-
-    double t = mean + k * std;
-
-    thresh = t_max > t ? t_max : t;
-    t_max = t_max > elapsed_time ? t_max : elapsed_time;
-    n++;
-
-    value_ptr->t_max = t_max;
-    value_ptr->n = n;
-    value_ptr->mean = mean;
-    value_ptr->std = std;
-    value_ptr->thresh = thresh;
-
-    bpf_printk("%llu %llu %f %f %f\n: ", t_max, n, mean, std, thresh);
-
-    bpf_map_update_elem(&thresh_maps, &pid, value_ptr, BPF_ANY);
+    if(elapsed_time > 4097191){
+        // bpf_printk("Attack: [%d] took %llu nano seconds operations[%llu]\n: ", pid, elapsed_time);
+    }
+    else{
+        // bpf_printk("Normal: [%d] took %llu nano seconds operations[%llu]\n: ", pid, elapsed_time);
+    }
 }
