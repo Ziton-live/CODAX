3fdbc53c22708486566e68a5a1e73af1762aba2b
NIRANJAN B <niranjannb7777@gmail.com>
2023-03-22 23:25:47 +0530

    Refactor Code

--- src/_monitor/container_tracer.h
+++ src/_monitor/container_tracer.h
@@ -13,7 +13,7 @@
  * @todo Find a more reliable method for detecting containerized processes.
  */
 int __is_it_docker(const char *str) {
-    return str[0]=='d' && str[1]=='o'&& str[2]=='c';
+    return str[0]=='p' && str[1]=='y'&& str[2]=='t';
 }
 
 
