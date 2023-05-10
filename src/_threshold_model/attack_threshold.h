0ceeaadd5a7e93c5095f18049bbca2cc533d4dba
NIRANJAN B <niranjannb7777@gmail.com>
2023-05-10 09:56:27 +0530

    List Exceed Thresh

--- src/_threshold_model/attack_threshold.h
+++ src/_threshold_model/attack_threshold.h
@@ -50,16 +50,16 @@ void __is_cont_list_exceed_threshold() {
         if (__pids[i] == 0) return;
         unsigned int threshold = __get_value_from_map((struct bpf_map *) &proc_pid_threshold_hash_map, __pids[i]);
         bpf_printk("Threshold(%d) = %i\n: ", __pids[i], threshold);
-        u64 *start_time = bpf_map_lookup_elem(&proc_pid_start_time_hash_map, &__pids[i]);
-        u64 end_time = __get_current_time();
-        if(start_time){
-            u64 elapsed_time = end_time - *start_time;
-            unsigned int st = (unsigned int) (elapsed_time & 0xFFFFFFFF);
-            if(st > threshold){
-                bpf_printk("Probable DOS %i\n: ", __pids[i]);
-
-            }
-        }
+//        u64 *start_time = bpf_map_lookup_elem(&proc_pid_start_time_hash_map, &__pids[i]);
+//        u64 end_time = __get_current_time();
+//        if(start_time){
+//            u64 elapsed_time = end_time - *start_time;
+//            unsigned int st = (unsigned int) (elapsed_time & 0xFFFFFFFF);
+//            if(st > threshold){
+//                bpf_printk("Probable DOS %i\n: ", __pids[i]);
+//
+//            }
+//        }
     }
 }
 
@@ -82,6 +82,7 @@ void __production(int pid){
     } else {
         bpf_printk("Normal Thresh = %i - %i\n: ", threshold, elapsed_t);
     }
+    __is_cont_list_exceed_threshold();
 }
 
 
@@ -108,13 +109,13 @@ int model_cpu_threshold(u64 elapsed_time, int pid) {
 
     bpf_printk("[%d] took %llu nano seconds\n: ", pid, elapsed_time);
 
-    bpf_printk("Count = %i\n: ", request_count);
-    bpf_printk("Mean = %i\n: ", mean_val);
-    bpf_printk("Std = %i\n: ", std_val);
-    bpf_printk("Max = %i\n: ", max_val);
-    bpf_printk("Thresh = %i\n: ", threshold);
-    bpf_printk("U64 = %i\n: ", elapsed_time);
-    bpf_printk("U32 = %i\n: ", elapsed_t);
+//    bpf_printk("Count = %i\n: ", request_count);
+//    bpf_printk("Mean = %i\n: ", mean_val);
+//    bpf_printk("Std = %i\n: ", std_val);
+//    bpf_printk("Max = %i\n: ", max_val);
+    bpf_printk("Thresh = %u\n: ", threshold);
+//    bpf_printk("U64 = %i\n: ", elapsed_time);
+//    bpf_printk("U32 = %i\n: ", elapsed_t);
 
 
     std_val = _sqrt((request_count * std_val * std_val + (elapsed_t - mean_val) * (elapsed_t - mean_val)) /
@@ -136,7 +137,6 @@ int model_cpu_threshold(u64 elapsed_time, int pid) {
     bpf_map_update_elem(&proc_pid_max_hash_map, &pid, &max_val, BPF_ANY);
     bpf_map_update_elem(&proc_pid_threshold_hash_map, &pid, &threshold, BPF_ANY);
     __add_pids(pid);
-    __is_cont_list_exceed_threshold();
     return 0;
 }
 
