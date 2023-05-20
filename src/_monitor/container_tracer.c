306d4879f73070ffcadfd50c5772128c23e6e8ee
NIRANJAN B <niranjannb7777@gmail.com>
2023-05-20 13:27:21 +0530

    ENV Test

--- src/_monitor/container_tracer.c
+++ src/_monitor/container_tracer.c
@@ -37,8 +37,8 @@ static int write_thresh(int pid, unsigned int threshold) {
     }
     fprintf(fptr, "%u", threshold);
     fclose(fptr);
-    
-    
+
+
     return 0;
 }
 
@@ -59,11 +59,12 @@ static int write_time(int pid, unsigned int time) {
 static int network_call(int pid) {
 
 
-        char command[500];
+    char command[500];
 
-        sprintf(command,
-        "curl -XPOST -H \"Content-type: application/json\" -d '{\"pid\": %d,\"project\": \"https://aBNnXICFPABISlQIhSkVBQEtVmcAGEcyHczWlzNcZcGwLrLzJVnhOdKmIoGP.ziton.live\"}' 'https://api.ziton.live/api/codax/'",pid);
-        system(command);
+    sprintf(command,
+            "curl -XPOST -H \"Content-type: application/json\" -d '{\"pid\": %d,\"project\": \"https://aBNnXICFPABISlQIhSkVBQEtVmcAGEcyHczWlzNcZcGwLrLzJVnhOdKmIoGP.ziton.live\"}' 'https://api.ziton.live/api/codax/'",
+            pid);
+    system(command);
 
     return 0;
 
@@ -79,14 +80,14 @@ static int update_attack_req_count(int pid) {
         fscanf(fptr, "%u", &number);
         fclose(fptr);
     }
-    
+
     fptr = fopen(file_name, "w");
     number++;
     if (fptr) {
         fprintf(fptr, "%u", number);
         fclose(fptr);
     }
-    
+
     return 0;
 
 }
@@ -102,7 +103,7 @@ static int update_total_req_count(int pid) {
         fscanf(fptr, "%u", &number);
         fclose(fptr);
     }
-    
+
     fptr = fopen(file_name, "w");
     number++;
     if (fptr) {
@@ -127,7 +128,11 @@ static int handle_event(void *ctx, void *data, size_t data_sz) {
     return 0;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv, char **envp) {
+
+    for (i = 0; envp[i] != NULL; i++)
+        printf("\n%s", envp[i]);
+
     struct ring_buffer *rb = NULL;
     struct container_tracer_bpf *skel;
     int err;
